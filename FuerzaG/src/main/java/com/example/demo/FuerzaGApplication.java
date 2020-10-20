8a5d4e1d3bfb7d9d9776983f40a85c5efa44f44d
MiguelC1908 <n00070174@upn.pe>
2020-10-20 13:24:03 -0500

    Commit inciial

---
+++ FuerzaG/src/main/java/com/example/demo/FuerzaGApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuerzaGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuerzaGApplication.class, args);
+	}
+
+}
